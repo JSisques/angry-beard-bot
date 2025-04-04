224e887c527f5c89c2dff2247f25d0cbea7fee8b
JSisques <javierplazasisques@gmail.com>
2025-04-04 18:42:08 +0200

    fix: Update ESLint commands in workflow files to use lint:fix
    - Changed the ESLint command from 'yarn lint' to 'yarn lint:fix' in the API, Docs, Landing, and Web workflow files to automatically fix linting issues.
    - Updated ESLint configuration in the API to include the 'unused-imports' plugin for better management of unused imports.

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:unused-imports/recommended'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
