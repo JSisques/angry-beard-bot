33491302e1973d3c4583fd15aea7ca55cfa77afb
JSisques <javierplazasisques@gmail.com>
2025-04-09 19:08:15 +0200

    feat: Enhance logging during application bootstrap
    - Added a log statement to indicate when the NestApplication is created, improving traceability during the application startup process.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const logger = new Logger('NestApplication');
   const app = await NestFactory.create(AppModule);
+  logger.log('NestApplication created');
 
   // Global configuration
   app.enableCors();
